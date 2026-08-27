eec868d06f60848c7d61bc933a915934c82ad32e
Mick Neple <mickeinar2@gmail.com>
2026-08-27 22:41:22 +0200

    Added ack and nack queues etc

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -16,6 +16,14 @@ const (
 	Transient
 )
 
+type AckType int
+
+const (
+	Ack AckType = iota
+	NackRequeue
+	NackDiscard
+)
+
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	b, err := json.Marshal(val)
@@ -41,13 +49,18 @@ func DeclareAndBind(
 	mqChan, err := conn.Channel()
 	handlers.FailOnError(err, "Failed to create channel for connection")
 
+	
+	mqTable := amqp.Table{
+		"x-dead-letter-exchange": "peril_dlx",
+	}
+
 	var mqQueue amqp.Queue
 	if queueType == Durable {
-		mqQueue, err = mqChan.QueueDeclare(queueName, true, false, false, false, nil)
+		mqQueue, err = mqChan.QueueDeclare(queueName, true, false, false, false, mqTable)
 		handlers.FailOnError(err, "Failed to create queue for channel")
 		}
 	if queueType == Transient {
-		mqQueue, err = mqChan.QueueDeclare(queueName, false, true, true, false, nil)
+		mqQueue, err = mqChan.QueueDeclare(queueName, false, true, true, false, mqTable)
 		handlers.FailOnError(err, "Failed to create queue for channel")
 		}
 
@@ -56,32 +69,43 @@ func DeclareAndBind(
 	return mqChan, mqQueue, nil
 	}
 
-func SubscribeJSON[T any](
+func SubscribeJSON[T any] (
     conn *amqp.Connection,
     exchange,
     queueName,
     key string,
     queueType SimpleQueueType, // an enum to represent "durable" or "transient"
-    handler func(T),
+    handler func(T) AckType, // an enum to return the ack type of the channel
 ) error {
 	ch, qu, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
 	if err != nil {
 		return fmt.Errorf("There was an issue establishing either channel: %v or queue: %v. error: %v", ch, qu, err)
 	}
 
-	delivery, err  := ch.Consume(qu.Name, "", false, false, false, false, nil)
+	messages, err  := ch.Consume(qu.Name, "", false, false, false, false, nil)
 	if err != nil {
-		return fmt.Errorf("There was an issue creating new channel %v, err: %v", delivery, err)
+		return fmt.Errorf("There was an issue creating new channel %v, err: %v", messages, err)
 	}
 		go func() {
-			for d := range delivery {
+			for msg := range messages {
 				var data T
-				err := json.Unmarshal(d.Body, &data)
+				err := json.Unmarshal(msg.Body, &data)
 				if err != nil {
 					return //fmt.Errorf("Failed to unmarshal channel data: %v", err)
 				}
-				handler(data)
-				d.Ack(false)
+
+				ackType := handler(data)
+				switch ackType {
+				case Ack:
+					msg.Ack(false)
+					fmt.Println("Ack")
+				case NackRequeue:
+					msg.Nack(false, true)
+					fmt.Println("NackRequeue")
+				case NackDiscard: 
+					msg.Nack(false, false)
+					fmt.Println("NackDiscard")
+				}
 			}
 		}()
 	return nil
